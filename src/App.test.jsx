54a8ef65dbf165359f5297dd3b32f0300f620d1e
Conor Kotwasinski <conorkotwasinski2024@u.northwestern.edu>
2023-11-20 20:19:29 -0600

    test 1

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,19 +1,36 @@
-import {describe, expect, test} from 'vitest';
-import {fireEvent, render, screen} from '@testing-library/react';
+import { describe, expect, test, vi } from 'vitest';
+import { render, screen } from '@testing-library/react';
 import App from './App';
+import * as firebaseUtils from './utilities/firebase';
+import '@testing-library/jest-dom';
 
-describe('counter tests', () => {
-    
-  test("Counter should be 0 at the start", () => {
+
+// Partially mock the firebase.js module
+vi.mock('./utilities/firebase', async () => {
+  const actual = await vi.importActual('./utilities/firebase'); // Import the actual module
+  return {
+    ...actual, // Keep all the original exports
+    useAuthState: vi.fn(), // Mock only useAuthState
+  };
+});
+
+describe('App and Header tests', () => {
+
+  test('shows Sign In if not logged in', () => {
+    // Mock useAuthState to return null user
+    firebaseUtils.useAuthState.mockReturnValue([null]);
+
     render(<App />);
-    expect(screen.getByText('count is: 0')).toBeDefined();
+    expect(screen.getByText('Sign In')).toBeInTheDocument();
   });
 
-  test("Counter should increment by one when clicked", async () => {
+  test('shows POST OPPORTUNITIES button when logged in', () => {
+    // Mock useAuthState to return a dummy user object
+    firebaseUtils.useAuthState.mockReturnValue([{ displayName: 'Joe', photoURL: 'url_to_dummy_profile_pic.png' }]);
+
     render(<App />);
-    const counter = screen.getByRole('button');
-    fireEvent.click(counter);
-    expect(await screen.getByText('count is: 1')).toBeDefined();
+    const postOpportunitiesButton = screen.getByText('POST OPPORTUNITIES');
+    expect(postOpportunitiesButton).toBeInTheDocument();
   });
 
 });
